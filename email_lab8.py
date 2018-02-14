af3809c81ecb09b5a2a2e8b5f9b2dcbb3b78a494
thundernithin <35676620+thundernithin@users.noreply.github.com>
2018-02-14 09:55:30 +0530

    Delete email_lab8.py

--- email_lab8.py
+++
@@ -1,9 +0,0 @@
-import smtplib
- 
-server = smtplib.SMTP('smtp.gmail.com',587)#start session
-server.starttls()#opening the session
-server.login("nithind7026@gmail.com", "7026378456")#logging into the account
-msg = "heyyyyy.....how art thou doing"#the actual message
-server.sendmail("nithind7026@gmail.com", "nischay.17cs@cmr.edu.in", msg)#sending the mail
-print("sucess")
-server.quit()#closing the server
